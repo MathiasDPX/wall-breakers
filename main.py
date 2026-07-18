6e5be23320fc51ac5fca71fa6052910afaa6b84a
MathiasDPX <mathias@dupeux.net>
2026-07-18 23:48:55 +0200

    fix: clean article content for LeMonde

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from providers import *
 
 
 if __name__ == "__main__":
-    article = LeParisienArticle.get_from_url("https://www.leparisien.fr/sports/football/coupe-du-monde/france-angleterre-la-composition-probable-des-bleus-avec-zaire-emery-cherki-olise-et-mbappe-18-07-2026-ZMLNSNIHBVGEPALOLJ3KGMBQAI.php")
-    print(article)
+    lp_article = LeParisienArticle.get_from_url("https://www.leparisien.fr/sports/football/coupe-du-monde/france-angleterre-la-composition-probable-des-bleus-avec-zaire-emery-cherki-olise-et-mbappe-18-07-2026-ZMLNSNIHBVGEPALOLJ3KGMBQAI.php")
+    print(lp_article)
--- providers/lemonde.py
+++ providers/lemonde.py
@@ -1,4 +1,6 @@
-from .common import Article
+from common import Article
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
 import re
 import requests
 
@@ -8,9 +10,7 @@ _URL_ID_PATTERN = re.compile(
 )
 
 
-
 class LeMondeArticle(Article):
-
     def __init__(self, article_id: str):
         r = requests.get(
             f"https://apps.lemonde.fr/aec/v1/premium-ios-tablet/article/{article_id}"
@@ -18,12 +18,45 @@ class LeMondeArticle(Article):
         r.raise_for_status()
         data = r.json()
 
+        soup = BeautifulSoup(data["template_vars"]["content"], features="html.parser").find_all(
+            "div", attrs={"class": "article_content"})[0]
+
+        # Remove See Also, Inread and PubStack containers
+        for container in soup.select("div.see-also-container, div.inread-container, div.pubstack-container"):
+            container.decompose()
+
+        for tag in soup.find_all():
+            # Remove unwanted attributes
+            tag.attrs.pop('style', None)
+            tag.attrs.pop('data-read-progression', None)
+            tag.attrs.pop('id', None)
+            tag.attrs.pop('class', None)
+            tag.attrs.pop('onload', None)
+            tag.attrs.pop('onerror', None)
+            tag.attrs.pop('"', None)
+
+            # Remove empty tags
+            if not tag.get_text(strip=True) and not tag.find() and tag.name not in ["img", "br", "hr", "input"]:
+                tag.decompose()
+
+        for img in soup.find_all("a", href=True):
+            url = img["href"].replace("lmfr://illustration?url=", "")
+            img["href"] = unquote(url)
+
+        for a in soup.find_all("a", role="button"):
+            a.unwrap()
+
+        for div in soup.select("figure > div"):
+            div.unwrap()
+
+        content = soup.decode_contents()
+
         super().__init__(
-            id="lemonde:"+article,
-            headline=data["template_vars"]["basic"],
+            id="lemonde:"+article_id,
+            headline=data["template_vars"]["seo_title"],
             subheadline=data["template_vars"]["share_kicker"],
-            content=data["template_vars"]["content"],
-            image=None, # TODO: get from og_metas
+            content=content,
+            image=None,
         )
 
     @classmethod
