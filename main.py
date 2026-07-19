ac346e6218c3fabd24e38f5ee18869b61eac7a6d
MathiasDPX <mathias@dupeux.net>
2026-07-19 03:23:05 +0200

    feat: add favicon

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 import sass
 from providers import *
+import os
 
 app = Flask(__name__)
 
 sass.compile(dirname=('./static/scss/', './static/css'))
 
+
+@app.route("/favicon.ico")
+def favicon_route():
+    return send_file(os.path.join("static", "images", "favicon.ico"))
 
 @app.route("/lm/<id>")
 def lemonde_route(id):
@@ -17,6 +22,10 @@ def lemonde_route(id):
 def leparisien_route(id):
     article = LeParisienArticle(id)
     return render_template("article.html", article=article)
+
+@app.route("/")
+def index_route():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
