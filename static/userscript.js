104db23f1b50660ccde8cf9bd3892568d914f2a0
MathiasDPX <mathias@dupeux.net>
2026-08-01 14:05:55 +0200

    fix: bump userscript version

--- static/userscript.js
+++ static/userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wall Breakers Redirect
 // @namespace    https://mathiasd.fr/
-// @version      1.0
+// @version      1.0.1
 // @description  Show a popup on article compatible with Wall Breakers
 // @author       MathiasDPX
 // @updateURL    https://news.mathiasd.fr/redirect.user.js
