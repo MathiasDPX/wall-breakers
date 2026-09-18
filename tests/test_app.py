a26802dc4339f8145a0c265117182be27b155e05
MathiasDPX <mathias@dupeux.net>
2026-09-18 20:26:40 +0200

    fix: disable the athletic :(
    added a captcha

--- tests/test_app.py
+++ tests/test_app.py
@@ -18,8 +18,8 @@ URLS = [
     "https://www.lemonde.fr/planete/article/2026/07/18/au-canada-les-feux-a-repetition-bouleversent-la-foret-boreale_6724998_3244.html",
     "https://www.letelegramme.fr/finistere/landerneau-29800/a-landerneau-une-journee-pour-celebrer-la-culture-bretonne-le-25-juillet-avec-fest-e-landerne-7086034.php",
     "https://www.lesechos.fr/monde/etats-unis/lespagne-gagne-la-coupe-du-monde-de-foot-et-la-fifa-empoche-un-pactole-2243070",
-    "https://www.nytimes.com/athletic/7444334/2026/07/16/gianni-infantino-fifa-president-future/",
-    #"https://www.nytimes.com/2026/07/25/opinion/boy-scouts-girls-gender.html", Disabled as it's not working due to Datadome anti-scraping protection
+    #"https://www.nytimes.com/athletic/7444334/2026/07/16/gianni-infantino-fifa-president-future/", # disabled cuz they added a captcha
+    #"https://www.nytimes.com/2026/07/25/opinion/boy-scouts-girls-gender.html", # Disabled as it's not working due to Datadome anti-scraping protection
     "https://www.washingtonpost.com/opinions/2026/07/26/christopher-nolan-odyssey-shows-cost-online-rage/",
     "https://www.lejdd.fr/culture/expositions-la-france-au-fil-de-lart-179928",
     "https://sante.lefigaro.fr/psychologie/complice-d-un-systeme-monstrueux-hans-asperger-le-psychiatre-qui-triait-les-enfants-pour-le-reich-20260728",
